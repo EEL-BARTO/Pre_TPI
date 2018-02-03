a8532c0e394cf00b14db154cd43084b0ed5dfd06
haduken <vincentfagioli@gmail.com>
2018-02-03 21:45:46 +0100

    écriture des donnés inscription dans la db
    Première écriture dans la db des données obtenue grâce au formulaire créer précédament un pseudo un password et un mail

--- connexion.php
+++ connexion.php
@@ -4,7 +4,7 @@
 // Date: 18.06.2016
 // Auteur:VFI  
 
-//########################## Traitement (POST) ############################################
+
 //########################## Connexion DB ############################################
 try
 {
@@ -16,13 +16,26 @@ catch(Exception $e)
 	// En cas d'erreur, on affiche un message et on arrête tout
         die('Erreur : '.$e->getMessage());
 }
+//########################## Traitement (POST) ############################################
+extract($_POST); // $pseudo , $password . $mail
+if(isset($pseudo) && isset($password) && isset($mail))
+{
+
+$stmt = $bdd->prepare('INSERT INTO `user` (`id_user`, `pseudo`, `password`, `mail`) VALUES (NULL, ?, ?, ?)');
+$stmt->bindParam(1, $pseudo);
+$stmt->bindParam(2, $password);
+$stmt->bindParam(3, $mail);
+$stmt->execute();
+
+ echo"<script language=\"javascript\"> alert('inscription réussie') </script>";
+}
 // ################################# Affichage du contenu de la page ##############################
 
-// On récupère tout le contenu de la table jeux_video
-$reponse = $bdd->query('SELECT * FROM user');
+// On récupère tout le contenu de la table jeux_video 
+/*$reponse = $bdd->query('SELECT * FROM user');
 
 // On affiche chaque entrée une à une
-/*while ($donnees = $reponse->fetch())
+while ($donnees = $reponse->fetch())
 { 
  echo'    <strong>Jeu</strong> : <?php echo $donnees["pseudo"]; ?><br />
     Le possesseur de ce jeu est : <?php echo $donnees["mail"]; ?>, et il le vend à <?php echo $donnees["password"]; ?> euros !<br />
@@ -34,12 +47,11 @@ $reponse = $bdd->query('SELECT * FROM user');
 $reponse->closeCursor(); // Termine le traitement de la requête
 
 */
-?> 
-<?php   echo '<form name="formulaire" method="post" action="connexion.php">
+   echo '<form name="formulaire" method="post" action="connexion.php">
                         <br><br><label>Nom:</label><br>
                         <input class="Test" type="text" size="40" name="pseudo" id="Name"><br><br>
                         <label>Mot de Passe:</label><br>
-                        <input type="password" size="40" name="paassword"> <br><br>
+                        <input type="password" size="40" name="password"> <br><br>
                         <label>Confirmer votre mot de passe:</label><br>
                         <input type="password" size="40" name="Confirmmot_de_passe"> <br><br>
                         <label>E-mail:</label><br>
--- index.php
+++ index.php
@@ -3,4 +3,5 @@
 // Date: 18.06.2016
 // Auteur:VFI 
 echo '<html><a href="connexion.php">lien</a></html>';
+
 ?>
