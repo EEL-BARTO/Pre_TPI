10ba095cdc6d730addcbd908054379720da6b787
haduken <vincentfagioli@gmail.com>
2018-02-04 12:05:07 +0100

    Vérification connexion
    Affiche ok si le couple password pseudo entré est trouvé dans la bdd sinon affiche not ok

--- connexion.php
+++ connexion.php
@@ -14,44 +14,38 @@ try
 catch(Exception $e)
 {
 	// En cas d'erreur, on affiche un message et on arrête tout
-        die('Erreur : '.$e->getMessage());
+    die('Erreur : '.$e->getMessage());
 }
 //########################## Traitement (POST) ############################################
-extract($_POST); // $pseudo , $password . $mail
+extract($_POST); // $pseudo , $password . $mail . $pseudo_connexion . $password_connexion
 if(isset($pseudo) && isset($password) && isset($mail))
 {
-
-$stmt = $bdd->prepare('INSERT INTO `user` (`id_user`, `pseudo`, `password`, `mail`) VALUES (NULL, ?, ?, ?)');
-$stmt->bindParam(1, $pseudo);
-$stmt->bindParam(2, $password);
-$stmt->bindParam(3, $mail);
-$stmt->execute();
-
- echo"<script language=\"javascript\"> alert('inscription réussie') </script>";
+    $stmt = $bdd->prepare('INSERT INTO `user` (`id_user`, `pseudo`, `password`, `mail`) VALUES (NULL, ?, ?, ?)');
+    $stmt->bindParam(1, $pseudo);
+    $stmt->bindParam(2, $password);
+    $stmt->bindParam(3, $mail);
+    $stmt->execute();
+    echo"<script language=\"javascript\"> alert('inscription réussie') </script>";
 }
+if(isset($password_connexion) && isset($password_connexion))
+{    
+        //On obtient une table existe qui contient le nombre d'entrée correspondant au couple pseudo password
+        $query = "SELECT COUNT(*) AS existe FROM user WHERE `pseudo` LIKE '$pseudo_connexion' AND `password` LIKE '$password_connexion' ";
+        $stmt = $bdd->query($query) or die ("<br>SQL Error in: <br> $query<br>Error message:".$bdd->errorInfo()[2]); 
+        $ident = $stmt->fetch();
+        if($ident['existe']>0)
+        {
+            echo'ok';
+        }
+        else
+        {
+        echo'not ok';
+        }
+}
+
 // ################################# Affichage du contenu de la page ##############################
 
-// On récupère tout le contenu de la table jeux_video 
-/*$reponse = $bdd->query('SELECT * FROM user');
-
-// On affiche chaque entrée une à une
-while ($donnees = $reponse->fetch())
-{ 
- echo'    <strong>Jeu</strong> : <?php echo $donnees["pseudo"]; ?><br />
-    Le possesseur de ce jeu est : <?php echo $donnees["mail"]; ?>, et il le vend à <?php echo $donnees["password"]; ?> euros !<br />
-    Ce jeu fonctionne sur et on peut y jouer à <?php echo $donnees["id_user"]; ?> au maximum<br />
-    /em>
-   </p>';
-}
-
-$reponse->closeCursor(); // Termine le traitement de la requête
-<input type="button" value="Afficher ou Maquer" onClick="masquer_div("a_masquer");"/>
-   <div id="a_masquer">
-
-*/
-
-
-   echo '<form name="formulaire" method="post" action="connexion.php">
+ echo '<form name="formulaire" method="post" action="connexion.php">
                         <br><br><label>Nom:</label><br>
                         <input class="Test" type="text" size="40" name="pseudo" id="Name"><br><br>
                         <label>Mot de Passe:</label><br>
@@ -65,9 +59,11 @@ $reponse->closeCursor(); // Termine le traitement de la requête
 
     echo'<form name="formulaire_connexion" method="post" action="connexion.php">
                         <br><br><label>Nom:</label><br>
-                        <input class="Test" type="text" size="40" name="pseudo_connexion" id="Name"><br><br>
+                        <input class="Test" type="text" size="40" name="pseudo_connexion" id="Name">*%%<br><br>
                         <label>Mot de Passe:</label><br>
                         <input type="password" size="40" name="password_connexion"><br><br><br>
                         <button type="submit">connexion</button>
                     </form>';
+
+
 ?>
